e6b42c1b573e3f8c04e379e7144d4edd0708000f
will4906 <553105821@qq.com>
2017-08-15 12:50:24 +0800

    Features:添加了requirements.txt

--- entity/QueryItem.py
+++ entity/QueryItem.py
@@ -12,13 +12,23 @@ class QueryItem:
         self.requestNumber = ""
         # 申请日
         self.requestDate = ""
-        # 公开号
+        # 公开（公告）号
         self.publishNumber = ""
-        # 公布日
+        # 公开（公告）日
         self.publishDate = ""
-
-
         # 申请（专利权）人
         self.proposerName = ""
         # 发明人
         self.inventorName = ""
+        # 优先权号
+        self.priorityNumber = ""
+        # 优先权日
+        self.priorityDate = ""
+        # 摘要
+        self.abstract = ""
+        # 权利要求
+        self.claim = ""
+        # 说明书
+        self.instructions = ""
+        # 关键词
+        self.keyword = ""
