59ae94fdda92df461e3e93a5893d3c5313feb9ed
will4906 <553105821@qq.com>
2017-09-11 03:50:11 +0800

    更新了专利检索式生成算法

--- entity/QueryItem.py
+++ entity/QueryItem.py
@@ -6,6 +6,7 @@ Created on 2017/3/19
 """
 
 
+# 处理item_group函数
 def handle_item_group(item_group):
     AND = ' AND '
     OR = ' OR '
@@ -34,10 +35,12 @@ def handle_item_group(item_group):
     return exp_str
 
 
+# 处理申请号的函数
 def handle_request_number(request_number):
     print(request_number)
 
 
+# 处理日期元素的函数
 def handle_date_element(title, date_element):
     if isinstance(date_element, DateSelect):
         return title + date_element.__getattribute__('search_exp')
@@ -45,6 +48,40 @@ def handle_date_element(title, date_element):
         raise Exception('We just support DateSelect for date element!')
 
 
+# 处理发明类型的函数
+def handle_invention_type(title, invention_type):
+    exp_str = ""
+    if isinstance(invention_type, Or):
+        OR = ' OR '
+        keyor = invention_type
+        if keyor is not None:
+            parms = keyor.__getattribute__('parm')
+            for parm in parms:
+                if parm == 'I' or parm == 'U' or parm == 'D':
+                    parm = '\"' + parm + '\"'
+                elif parm.find('发明申请') != -1:
+                    parm = '\"I\"'
+                elif parm.find('实用新型') != -1:
+                    parm = '\"U\"'
+                elif parm.find('外观设计') != -1:
+                    parm = '\"D\"'
+                exp_str += OR + parm
+            exp_str = exp_str.replace(OR, '', 1)
+    elif isinstance(invention_type, str):
+        if invention_type == 'I' or invention_type == 'U' or invention_type == 'D':
+            exp_str = '\"' + invention_type + '\"'
+        elif invention_type.find('发明申请') != -1:
+            exp_str = '\"I\"'
+        elif invention_type.find('实用新型') != -1:
+            exp_str = '\"U\"'
+        elif invention_type.find('外观设计') != -1:
+            exp_str = '\"D\"'
+    else:
+        raise Exception('We just support string or Or for date element!')
+    return "(" + exp_str + ")"
+
+
+# 默认处理函数
 def default_handle(title, default):
     if isinstance(default, ItemGroup):
         return title + '=(' + handle_item_group(default) + ')'
@@ -52,6 +89,33 @@ def default_handle(title, default):
         return title + '=(' + default + ')'
     else:
         raise Exception('We just support string or ItemGroup!')
+
+
+def find_element_in_item_group(element, item_group):
+    keyand = item_group.__getattribute__('And')
+    keyor = item_group.__getattribute__('Or')
+    keynot = item_group.__getattribute__('Not')
+    if keyand is not None:
+        parms = keyand.__getattribute__('parm')
+        try:
+            return parms.index(element)
+        except:
+            pass
+    if keyor is not None:
+        parms = keyor.__getattribute__('parm')
+        try:
+            return parms.index(element)
+        except:
+            pass
+    if keynot is not None:
+        parms = keynot.__getattribute__('parm')
+        try:
+            return parms.index(element)
+        except:
+            pass
+    return None
+
+
 
 title_case = {
     'request_number': default_handle,
@@ -84,18 +148,18 @@ title_case = {
     'German_invention_name': default_handle,
     'other_invention_name': default_handle,
     'English_abstract': default_handle,
-    'PCT_enters_national_phase_date': default_handle,
+    'PCT_enters_national_phase_date': handle_date_element,
     'PCT_international_application_number': default_handle,
     'French_abstract': default_handle,
     'German_abstract': default_handle,
     'other_abstract': default_handle,
-    'PCT_international_application_date': default_handle,
+    'PCT_international_application_date': handle_date_element,
     'PCT_international_publish_number': default_handle,
-    'PCT_international_publish_date': default_handle,
+    'PCT_international_publish_date': handle_date_element,
     'CPC_class_number': default_handle,
     'C-SETS': default_handle,
+    'invention_type': handle_invention_type,
     'publish_country': default_handle,
-    'invention_type': default_handle
 }
 title_define = {
     'request_number': '申请号',
@@ -138,8 +202,8 @@ title_define = {
     'PCT_international_publish_date': 'PCT国际申请公开日期',
     'CPC_class_number': 'CPC分类号',
     'C-SETS': 'C-SETS',
+    'invention_type': '发明类型',
     'publish_country': '公开国',
-    'invention_type': '发明类型'
 }
 
 
@@ -172,6 +236,11 @@ class ItemGroup:
         self.Or = Or
         self.Not = Not
 
+    def add_or(self, *parm):
+        if self.Or is None:
+            self.Or = Or(*parm)
+        else:
+            self.Or.add_parm(*parm)
 
 class And:
     def __init__(self, *parm):
@@ -185,6 +254,9 @@ class Or:
     def __init__(self, *parm):
         self.parm = parm
 
+    def add_parm(self, *ps):
+        self.parm = self.parm + ps
+
 
 class Not:
     def __init__(self, *parm):
@@ -195,6 +267,20 @@ class QueryItem:
     def __init__(self, **kwargs):
         self.parm = kwargs
         self.queryAnd = And()
+
+        invention_type = kwargs.get('invention_type')
+        if invention_type is not None:
+            publish_country = kwargs.get('publish_country')
+            if publish_country is None:
+                kwargs['publish_country'] = 'CN'
+            else:
+                if isinstance(publish_country, str):
+                    if publish_country != 'CN':
+                        kwargs['publish_country'] = ItemGroup(Or=Or('CN', publish_country))
+                elif isinstance(publish_country, ItemGroup):
+                    if find_element_in_item_group('CN', publish_country) is None:
+                        publish_country.add_or('CN')
+
         for title, value in title_define.items():
             key = kwargs.get(title)
             if key is not None:
