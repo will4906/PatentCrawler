1034c6315856b4ad28aa3cc0ee7e445db423e094
will4906 <553105821@qq.com>
2017-09-29 01:26:33 +0800

    修改点东西

--- config/BaseConfig.py
+++ config/BaseConfig.py
@@ -9,7 +9,7 @@ from util.TimeUtil import TimeUtil
 
 class BaseConfig:
     # 爬取专利速度，每个请求80个，范围是(0,89]
-    CRAWLER_SPEED = "80"
+    CRAWLER_SPEED = "12"
     FILE_NAME = "output\专利.xls"
     LOG_FILE_NAME = "log\PatentCrawler{0}.log".format(TimeUtil.getFormatTime("%Y%m%d_%H%M%S"))
     TEMP_DIR_NAME = "temp_save"
